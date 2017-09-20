2003e92fc79a7245bb14d2d12c17dd211dec4895
Roshan Choxi <choxi@users.noreply.github.com>
2017-09-19 21:35:12 -0500

    Update Circle.js

--- src/Circle.js
+++ src/Circle.js
@@ -5,9 +5,9 @@ import enhancer from './enhancer';
 import { propTypes, defaultProps } from './types';
 
 /**
- *  @name Line
+ *  @name Circle
  *  @example
- *  <Line percent="10" strokeWidth="4" strokeColor="#D3D3D3" />
+ *  <Circle percent="10" strokeWidth="4" strokeColor="#D3D3D3" />
  */
 class Circle extends Component {
   getPathStyles() {
